c483f779f046fcfaeebae8e08b83cca364aacd72
Clément Ollivier <clement.o2p@gmail.com>
2018-06-01 15:50:21 +0200

    :sparkles: Add GET route for produts and product

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -33,3 +33,13 @@ module.exports.addOrder = (req, res) => {
     slot
   });
 };
+
+module.exports.getProducts = async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
+};
+
+module.exports.getProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.send(product);
+};
